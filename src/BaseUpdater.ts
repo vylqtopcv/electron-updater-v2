93c08ad2923118604a56b1a14ee7f484181938e4
vylq <vylq@topcv.vn>
2022-05-06 18:01:38 +0700

    init

---
+++ src/BaseUpdater.ts
@@ -0,0 +1,107 @@
+import { AllPublishOptions } from "builder-util-runtime"
+import { AppAdapter } from "./AppAdapter"
+import { AppUpdater, DownloadExecutorTask } from "./AppUpdater"
+
+export abstract class BaseUpdater extends AppUpdater {
+  protected quitAndInstallCalled = false
+  private quitHandlerAdded = false
+
+  protected constructor(options?: AllPublishOptions | null, app?: AppAdapter) {
+    super(options, app)
+  }
+
+  quitAndInstall(isSilent = false, isForceRunAfter = false): void {
+    this._logger.info(`Install on explicit quitAndInstall`)
+    const isInstalled = this.install(isSilent, isSilent ? isForceRunAfter : true)
+    if (isInstalled) {
+      setImmediate(() => {
+        // this event is normally emitted when calling quitAndInstall, this emulates that
+        require("electron").autoUpdater.emit("before-quit-for-update")
+        this.app.quit()
+      })
+    } else {
+      this.quitAndInstallCalled = false
+    }
+  }
+
+  protected executeDownload(taskOptions: DownloadExecutorTask): Promise<Array<string>> {
+    return super.executeDownload({
+      ...taskOptions,
+      done: event => {
+        this.dispatchUpdateDownloaded(event)
+        this.addQuitHandler()
+        return Promise.resolve()
+      },
+    })
+  }
+
+  // must be sync
+  protected abstract doInstall(options: InstallOptions): boolean
+
+  // must be sync (because quit even handler is not async)
+  protected install(isSilent: boolean, isForceRunAfter: boolean): boolean {
+    if (this.quitAndInstallCalled) {
+      this._logger.warn("install call ignored: quitAndInstallCalled is set to true")
+      return false
+    }
+
+    const downloadedUpdateHelper = this.downloadedUpdateHelper
+    const installerPath = downloadedUpdateHelper == null ? null : downloadedUpdateHelper.file
+    const downloadedFileInfo = downloadedUpdateHelper == null ? null : downloadedUpdateHelper.downloadedFileInfo
+    if (installerPath == null || downloadedFileInfo == null) {
+      this.dispatchError(new Error("No valid update available, can't quit and install"))
+      return false
+    }
+
+    // prevent calling several times
+    this.quitAndInstallCalled = true
+
+    try {
+      this._logger.info(`Install: isSilent: ${isSilent}, isForceRunAfter: ${isForceRunAfter}`)
+      return this.doInstall({
+        installerPath,
+        isSilent,
+        isForceRunAfter,
+        isAdminRightsRequired: downloadedFileInfo.isAdminRightsRequired,
+      })
+    } catch (e) {
+      this.dispatchError(e)
+      return false
+    }
+  }
+
+  protected addQuitHandler(): void {
+    if (this.quitHandlerAdded || !this.autoInstallOnAppQuit) {
+      return
+    }
+
+    this.quitHandlerAdded = true
+
+    this.app.onQuit(exitCode => {
+      if (this.quitAndInstallCalled) {
+        this._logger.info("Update installer has already been triggered. Quitting application.")
+        return
+      }
+
+      if (!this.autoInstallOnAppQuit) {
+        this._logger.info("Update will not be installed on quit because autoInstallOnAppQuit is set to false.")
+        return
+      }
+
+      if (exitCode !== 0) {
+        this._logger.info(`Update will be not installed on quit because application is quitting with exit code ${exitCode}`)
+        return
+      }
+
+      this._logger.info("Auto install update on quit")
+      this.install(true, false)
+    })
+  }
+}
+
+export interface InstallOptions {
+  readonly installerPath: string
+  readonly isSilent: boolean
+  readonly isForceRunAfter: boolean
+  readonly isAdminRightsRequired: boolean
+}
---
+++ src/main.ts
@@ -0,0 +1,112 @@
+import { CancellationToken, PackageFileInfo, ProgressInfo, UpdateFileInfo, UpdateInfo } from "builder-util-runtime"
+import { EventEmitter } from "events"
+import { URL } from "url"
+import { AppUpdater } from "./AppUpdater"
+import { LoginCallback } from "./electronHttpExecutor"
+
+export { AppUpdater, NoOpLogger } from "./AppUpdater"
+export { CancellationToken, PackageFileInfo, ProgressInfo, UpdateFileInfo, UpdateInfo }
+export { Provider } from "./providers/Provider"
+export { AppImageUpdater } from "./AppImageUpdater"
+export { MacUpdater } from "./MacUpdater"
+export { NsisUpdater } from "./NsisUpdater"
+
+// autoUpdater to mimic electron bundled autoUpdater
+let _autoUpdater: any
+
+// required for jsdoc
+export declare const autoUpdater: AppUpdater
+
+function doLoadAutoUpdater(): AppUpdater {
+  // tslint:disable:prefer-conditional-expression
+  if (process.platform === "win32") {
+    _autoUpdater = new (require("./NsisUpdater").NsisUpdater)()
+  } else if (process.platform === "darwin") {
+    _autoUpdater = new (require("./MacUpdater").MacUpdater)()
+  } else {
+    _autoUpdater = new (require("./AppImageUpdater").AppImageUpdater)()
+  }
+  return _autoUpdater
+}
+
+Object.defineProperty(exports, "autoUpdater", {
+  enumerable: true,
+  get: () => {
+    return _autoUpdater || doLoadAutoUpdater()
+  },
+})
+
+export interface ResolvedUpdateFileInfo {
+  readonly url: URL
+  readonly info: UpdateFileInfo
+
+  packageInfo?: PackageFileInfo
+}
+
+export interface UpdateCheckResult {
+  readonly updateInfo: UpdateInfo
+
+  readonly downloadPromise?: Promise<Array<string>> | null
+
+  readonly cancellationToken?: CancellationToken
+
+  /** @deprecated */
+  readonly versionInfo: UpdateInfo
+}
+
+export type UpdaterEvents = "login" | "checking-for-update" | "update-available" | "update-not-available" | "update-cancelled" | "download-progress" | "update-downloaded" | "error"
+
+export const DOWNLOAD_PROGRESS = "download-progress"
+export const UPDATE_DOWNLOADED = "update-downloaded"
+
+export type LoginHandler = (authInfo: any, callback: LoginCallback) => void
+
+export class UpdaterSignal {
+  constructor(private emitter: EventEmitter) {}
+
+  /**
+   * Emitted when an authenticating proxy is [asking for user credentials](https://github.com/electron/electron/blob/master/docs/api/client-request.md#event-login).
+   */
+  login(handler: LoginHandler): void {
+    addHandler(this.emitter, "login", handler)
+  }
+
+  progress(handler: (info: ProgressInfo) => void): void {
+    addHandler(this.emitter, DOWNLOAD_PROGRESS, handler)
+  }
+
+  updateDownloaded(handler: (info: UpdateDownloadedEvent) => void): void {
+    addHandler(this.emitter, UPDATE_DOWNLOADED, handler)
+  }
+
+  updateCancelled(handler: (info: UpdateInfo) => void): void {
+    addHandler(this.emitter, "update-cancelled", handler)
+  }
+}
+
+export interface UpdateDownloadedEvent extends UpdateInfo {
+  downloadedFile: string
+}
+
+const isLogEvent = false
+
+function addHandler(emitter: EventEmitter, event: UpdaterEvents, handler: (...args: Array<any>) => void): void {
+  if (isLogEvent) {
+    emitter.on(event, (...args: Array<any>) => {
+      console.log("%s %s", event, args)
+      handler(...args)
+    })
+  } else {
+    emitter.on(event, handler)
+  }
+}
+
+export interface Logger {
+  info(message?: any): void
+
+  warn(message?: any): void
+
+  error(message?: any): void
+
+  debug?(message: string): void
+}
